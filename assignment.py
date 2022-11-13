91dc51b0803e0bb5e990f0812bd942ee9b9a443c
MickyMori <102163784+MickyMori@users.noreply.github.com>
2022-11-13 16:38:42 +0100

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -9,10 +9,16 @@ a_th = 2.0
 
 d_th = 0.4
 
+'''This list is used to keep track of which silver marker have already been grabbed'''
 silver_token_list = []
 
+
+'''This list is used to keep track of which golden marker are already paired with a silver one'''
 golden_token_list = []
 
+
+'''drive is a function that makes the robot move forward or backwards, depending on the sign of the 
+   speed value, for a certain amount of time specified by the variable seconds'''
 def drive(speed, seconds):
     
     R.motors[0].m0.power = speed
@@ -22,7 +28,8 @@ def drive(speed, seconds):
     R.motors[0].m1.power = 0
     
     
-
+'''turn is a function that makes the robot turn left or right, depending on the sign of the 
+   speed value, for a certain amount of time specified by the variable seconds'''
 def turn(speed, seconds):
     
     R.motors[0].m0.power = speed
@@ -32,7 +39,9 @@ def turn(speed, seconds):
     R.motors[0].m1.power = 0
     
     
-
+'''The find_silver_token function is used to locate the closest silver marker not already used 
+   that the robot sees. The funtion returns the distance and the angle in respect to the y-axis 
+   at which the marker is. It also returns the id of the marker.'''
 def find_silver_token():
     
     dist=100
@@ -47,7 +56,9 @@ def find_silver_token():
    	return dist, rot_y, code
    	
    	
-   	
+'''The find_golden_token function is used to locate the closest golden marker not already used 
+   that the robot sees. The funtion returns the distance and the angle in respect to the y-axis 
+   at which the marker is. It also returns the id of the marker.'''   	
 def find_golden_token():
     
     dist=100
@@ -62,7 +73,11 @@ def find_golden_token():
    	return dist, rot_y, code
    	
    	
-
+'''The grab_silver_token fuction calls the find_silver_function. If the robot sees a silver
+   marker that is not been already used it will get closer to it using the drive function to
+   move forward and the turn function to adjust its rotation as it proceeds. If the robot doesn't
+   see any new silver marker it will turn a bit to the right and look again. Once the robot is
+   close enough to the marker it will grab it.'''
 def grab_silver_token():
 	dist, rot_y, code = find_silver_token()
 	while 1:
@@ -71,11 +86,9 @@ def grab_silver_token():
 			turn(+10, 1)
 		elif dist <d_th: 
         		print("Found it!")
-        		R.grab()
-        		silver_token_list.append(code)
-        		print(silver_token_list)
-        		print(golden_token_list)
-        		return
+        		if R.grab():
+        			silver_token_list.append(code)
+        			return
         	elif -a_th<= rot_y <= a_th: 
 			print("Ah, that'll do.")
         		drive(20, 0.5)
@@ -88,7 +101,11 @@ def grab_silver_token():
         	dist, rot_y, code = find_silver_token()
         	
         	
-        	
+'''The go_close_to_golden_token fuction calls the find_golden_function. If the robot sees a golden
+   marker that is not been already used it will get closer to it using the drive function to
+   move forward and the turn function to adjust its rotation as it proceeds. If the robot doesn't
+   see any new golden marker it will turn a bit to the right and look again. Once the robot is
+   close enough to the marker it will release the silver token previously grabbed.'''        	
 def go_close_to_golden_token():
 	dist, rot_y, code = find_golden_token()
 	while 1:
@@ -97,11 +114,9 @@ def go_close_to_golden_token():
 			turn(+10, 1)
 		elif dist <d_th*1.5: 
         		print("Found it!")
-        		R.release()
-        		golden_token_list.append(code)
-        		print(silver_token_list)
-        		print(golden_token_list)
-        		return
+        		if R.release():
+        			golden_token_list.append(code)
+        			return
         	elif -a_th<= rot_y <= a_th: 
 			print("Ah, that'll do.")
         		drive(20, 0.5)
@@ -113,7 +128,11 @@ def go_close_to_golden_token():
         		turn(+2, 0.5)
 		dist, rot_y, code = find_golden_token()
 
-
+'''The main body of the program consits in a while loop that calls the grab_silver_token and
+   go_close_to_golden_token. Once the robot released the silver marker it will drive backwards
+   a bit to avoid dragging the marker around as it proceeds towards the next one. Once all six
+   golden markers have been used, and thus the golden_token_list's length is equal to six, the
+   program will break out of the loop.'''
 while 1:
     grab_silver_token()
     go_close_to_golden_token()
